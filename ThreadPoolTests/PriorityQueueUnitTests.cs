a4ebd4d4dc6c8f4b9e452f051568a1db9f7e43a6
Andrey Leskov <andrey.leskov@gmail.com>
2012-11-22 01:56:10 +0400

    tests fixed

--- ThreadPoolTests/PriorityQueueUnitTests.cs
+++ ThreadPoolTests/PriorityQueueUnitTests.cs
@@ -15,9 +15,9 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.Low, 2);
 			queue.Enqueue(Priority.Low, 3);
 
-			Assert.AreEqual(1, queue.Dequeue());
-			Assert.AreEqual(2, queue.Dequeue());
-			Assert.AreEqual(3, queue.Dequeue());
+			Assert.AreEqual(1, queue.Dequeue().Value);
+			Assert.AreEqual(2, queue.Dequeue().Value);
+			Assert.AreEqual(3, queue.Dequeue().Value);
 
 		}
 
@@ -29,9 +29,9 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.High, 2);
 			queue.Enqueue(Priority.High, 3);
 
-			Assert.AreEqual(1, queue.Dequeue());
-			Assert.AreEqual(2, queue.Dequeue());
-			Assert.AreEqual(3, queue.Dequeue());
+			Assert.AreEqual(1, queue.Dequeue().Value);
+			Assert.AreEqual(2, queue.Dequeue().Value);
+			Assert.AreEqual(3, queue.Dequeue().Value);
 
 		}
 
@@ -43,9 +43,9 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.Normal, 2);
 			queue.Enqueue(Priority.Normal, 3);
 
-			Assert.AreEqual(1, queue.Dequeue());
-			Assert.AreEqual(2, queue.Dequeue());
-			Assert.AreEqual(3, queue.Dequeue());
+			Assert.AreEqual(1, queue.Dequeue().Value);
+			Assert.AreEqual(2, queue.Dequeue().Value);
+			Assert.AreEqual(3, queue.Dequeue().Value);
 
 		}
 
@@ -62,13 +62,13 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.High, 6);
 			queue.Enqueue(Priority.High, 7);
 
-			Assert.AreEqual(5, queue.Dequeue());
-			Assert.AreEqual(6, queue.Dequeue());
-			Assert.AreEqual(7, queue.Dequeue());
-			Assert.AreEqual(3, queue.Dequeue());
-			Assert.AreEqual(4, queue.Dequeue());
-			Assert.AreEqual(1, queue.Dequeue());
-			Assert.AreEqual(2, queue.Dequeue());
+			Assert.AreEqual(5, queue.Dequeue().Value);
+			Assert.AreEqual(6, queue.Dequeue().Value);
+			Assert.AreEqual(7, queue.Dequeue().Value);
+			Assert.AreEqual(3, queue.Dequeue().Value);
+			Assert.AreEqual(4, queue.Dequeue().Value);
+			Assert.AreEqual(1, queue.Dequeue().Value);
+			Assert.AreEqual(2, queue.Dequeue().Value);
 		}
 
 		[TestMethod]
@@ -84,13 +84,13 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.Low, 6);
 			queue.Enqueue(Priority.Low, 7);
 
-			Assert.AreEqual(1, queue.Dequeue());
-			Assert.AreEqual(2, queue.Dequeue());
-			Assert.AreEqual(3, queue.Dequeue());
-			Assert.AreEqual(4, queue.Dequeue());
-			Assert.AreEqual(5, queue.Dequeue());
-			Assert.AreEqual(6, queue.Dequeue());
-			Assert.AreEqual(7, queue.Dequeue());
+			Assert.AreEqual(1, queue.Dequeue().Value);
+			Assert.AreEqual(2, queue.Dequeue().Value);
+			Assert.AreEqual(3, queue.Dequeue().Value);
+			Assert.AreEqual(4, queue.Dequeue().Value);
+			Assert.AreEqual(5, queue.Dequeue().Value);
+			Assert.AreEqual(6, queue.Dequeue().Value);
+			Assert.AreEqual(7, queue.Dequeue().Value);
 		}
 
 		[TestMethod]
@@ -108,15 +108,15 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.High, 8);
 			queue.Enqueue(Priority.High, 9);
 
-			Assert.AreEqual(5, queue.Dequeue());
-			Assert.AreEqual(6, queue.Dequeue());
-			Assert.AreEqual(7, queue.Dequeue());
-			Assert.AreEqual(3, queue.Dequeue());
-			Assert.AreEqual(8, queue.Dequeue());
-			Assert.AreEqual(9, queue.Dequeue());
-			Assert.AreEqual(4, queue.Dequeue());
-			Assert.AreEqual(1, queue.Dequeue());
-			Assert.AreEqual(2, queue.Dequeue());
+			Assert.AreEqual(5, queue.Dequeue().Value);
+			Assert.AreEqual(6, queue.Dequeue().Value);
+			Assert.AreEqual(7, queue.Dequeue().Value);
+			Assert.AreEqual(3, queue.Dequeue().Value);
+			Assert.AreEqual(8, queue.Dequeue().Value);
+			Assert.AreEqual(9, queue.Dequeue().Value);
+			Assert.AreEqual(4, queue.Dequeue().Value);
+			Assert.AreEqual(1, queue.Dequeue().Value);
+			Assert.AreEqual(2, queue.Dequeue().Value);
 		}
 
 		[TestMethod]
@@ -132,12 +132,12 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.High,   6);
 
 			
-			Assert.AreEqual(3, queue.Dequeue());
-			Assert.AreEqual(6, queue.Dequeue());
-			Assert.AreEqual(2, queue.Dequeue());
-			Assert.AreEqual(5, queue.Dequeue());
-			Assert.AreEqual(1, queue.Dequeue());
-			Assert.AreEqual(4, queue.Dequeue());
+			Assert.AreEqual(3, queue.Dequeue().Value);
+			Assert.AreEqual(6, queue.Dequeue().Value);
+			Assert.AreEqual(2, queue.Dequeue().Value);
+			Assert.AreEqual(5, queue.Dequeue().Value);
+			Assert.AreEqual(1, queue.Dequeue().Value);
+			Assert.AreEqual(4, queue.Dequeue().Value);
 		}
 
 		[TestMethod]
@@ -150,10 +150,10 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.High, 3);
 			queue.Enqueue(Priority.High, 4);
 
-			Assert.AreEqual(2, queue.Dequeue()); //high
-			Assert.AreEqual(3, queue.Dequeue()); //high
-			Assert.AreEqual(4, queue.Dequeue()); //high
-			Assert.AreEqual(1, queue.Dequeue()); //normal
+			Assert.AreEqual(2, queue.Dequeue().Value); //high
+			Assert.AreEqual(3, queue.Dequeue().Value); //high
+			Assert.AreEqual(4, queue.Dequeue().Value); //high
+			Assert.AreEqual(1, queue.Dequeue().Value); //normal
 		}
 
 		[TestMethod]
@@ -169,13 +169,13 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.High, 6);
 			queue.Enqueue(Priority.High, 7);
 
-			Assert.AreEqual(1, queue.Dequeue()); //high
-			Assert.AreEqual(2, queue.Dequeue()); //high
-			Assert.AreEqual(4, queue.Dequeue()); //high
-			Assert.AreEqual(5, queue.Dequeue()); //normal
-			Assert.AreEqual(6, queue.Dequeue()); //high
-			Assert.AreEqual(3, queue.Dequeue()); //high
-			Assert.AreEqual(7, queue.Dequeue()); //high
+			Assert.AreEqual(1, queue.Dequeue().Value); //high
+			Assert.AreEqual(2, queue.Dequeue().Value); //high
+			Assert.AreEqual(4, queue.Dequeue().Value); //high
+			Assert.AreEqual(5, queue.Dequeue().Value); //normal
+			Assert.AreEqual(6, queue.Dequeue().Value); //high
+			Assert.AreEqual(3, queue.Dequeue().Value); //high
+			Assert.AreEqual(7, queue.Dequeue().Value); //high
 		}
 		[TestMethod]
 		public void SimpleHighAndLowTest()
@@ -187,10 +187,10 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.Low, 3);
 			queue.Enqueue(Priority.High, 4);
 
-			Assert.AreEqual(1, queue.Dequeue()); //high
-			Assert.AreEqual(2, queue.Dequeue()); //high
-			Assert.AreEqual(4, queue.Dequeue()); //high
-			Assert.AreEqual(3, queue.Dequeue()); //normal
+			Assert.AreEqual(1, queue.Dequeue().Value); //high
+			Assert.AreEqual(2, queue.Dequeue().Value); //high
+			Assert.AreEqual(4, queue.Dequeue().Value); //high
+			Assert.AreEqual(3, queue.Dequeue().Value); //normal
 		}
 
 
@@ -215,21 +215,21 @@ namespace ThreadPoolTests
 			queue.Enqueue(Priority.High, 14);
 			queue.Enqueue(Priority.Low, 15);
 
-			Assert.AreEqual(3, queue.Dequeue()); //high
-			Assert.AreEqual(5, queue.Dequeue()); //high
-			Assert.AreEqual(6, queue.Dequeue()); //high
-			Assert.AreEqual(2, queue.Dequeue()); //normal
-			Assert.AreEqual(8, queue.Dequeue()); //high
-			Assert.AreEqual(9, queue.Dequeue()); //high
-			Assert.AreEqual(10, queue.Dequeue());//high
-			Assert.AreEqual(7, queue.Dequeue()); //normal
-			Assert.AreEqual(14, queue.Dequeue());//high
-			Assert.AreEqual(13, queue.Dequeue());//normal
-			Assert.AreEqual(1, queue.Dequeue()); //low
-			Assert.AreEqual(4, queue.Dequeue()); //low
-			Assert.AreEqual(11, queue.Dequeue());//low
-			Assert.AreEqual(12, queue.Dequeue());//low
-			Assert.AreEqual(15, queue.Dequeue());//low
+			Assert.AreEqual(3, queue.Dequeue().Value); //high
+			Assert.AreEqual(5, queue.Dequeue().Value); //high
+			Assert.AreEqual(6, queue.Dequeue().Value); //high
+			Assert.AreEqual(2, queue.Dequeue().Value); //normal
+			Assert.AreEqual(8, queue.Dequeue().Value); //high
+			Assert.AreEqual(9, queue.Dequeue().Value); //high
+			Assert.AreEqual(10, queue.Dequeue().Value);//high
+			Assert.AreEqual(7, queue.Dequeue().Value); //normal
+			Assert.AreEqual(14, queue.Dequeue().Value);//high
+			Assert.AreEqual(13, queue.Dequeue().Value);//normal
+			Assert.AreEqual(1, queue.Dequeue().Value); //low
+			Assert.AreEqual(4, queue.Dequeue().Value); //low
+			Assert.AreEqual(11, queue.Dequeue().Value);//low
+			Assert.AreEqual(12, queue.Dequeue().Value);//low
+			Assert.AreEqual(15, queue.Dequeue().Value);//low
 		}
 	}
 }
